1cb1a357f40592a2858bdbcd3f529d0428928b95
Dima Gerasimov <karlicoss@gmail.com>
2019-09-21 18:53:38 +0100

    Move tests to doctest

--- src/inorganic.py
+++ src/inorganic.py
@@ -3,7 +3,7 @@ import logging
 from pathlib import Path
 import re
 from collections import OrderedDict
-from typing import List, Optional, Dict, Union
+from typing import List, Optional, Dict, Union, Sequence
 
 Dateish = Union[datetime, date]
 
@@ -17,9 +17,14 @@ def sanitize_org_body(text: str) -> str:
 
 
 def sanitize_tag(tag: str) -> str:
+    """
+    >>> sanitize_tag('test-d@shes')
+    'test_d@shes'
+    """
     # https://orgmode.org/manual/Tags.html
     # Tags are normal words containing letters, numbers, ‘_’, and ‘@’.
     # TODO not sure, perhaps we want strict mode for formatting?
+    # TODO reuse orgparse regexes?
     return re.sub(r'[^@\w]', '_', tag)
 
 def link(url: Optional[str]=None, title: Optional[str]=None) -> str:
@@ -38,6 +43,11 @@ def datetime2orgtime(t: datetime) -> str:
     return t.strftime("%H:%M")
 
 def datetime2org(t: Dateish) -> str:
+    """
+    >>> dt = datetime.strptime('19920110 04:45', '%Y%m%d %H:%M')
+    >>> datetime2org(dt)
+    '1992-01-10 Fri 04:45'
+    """
     r = date2org(t)
     if isinstance(t, datetime):
         r += " " + datetime2orgtime(t)
@@ -53,14 +63,9 @@ def org_dt(t: Dateish, inactive=False, active=False) -> str:
     return beg + datetime2org(t) + end
 
 
-def test_datetime2org():
-    d = datetime.strptime('19920110 04:45', '%Y%m%d %H:%M')
-    assert datetime2org(d) == '1992-01-10 Fri 04:45'
-
 # TODO priority maybe??
 # TODO need to sanitize!
 # TODO for sanitizing, have two strategies: error and replace
-from typing import Sequence
 def as_org_entry(
         heading: Optional[str] = None,
         todo: Optional[str] = None,
@@ -68,25 +73,32 @@ def as_org_entry(
         scheduled: Optional[Dateish] = None,
         properties=None,
         body: Optional[str] = None,
-        created: Optional[Dateish]=None,
-        inline_created=False,
-        active_created=False,
-        force_no_created=False,
         level=1,
 ):
-    assert created is None, 'created is deprecated'
-    # TODO FIXME don't do that?
+    r"""
+    >>> as_org_entry(heading=None, tags=['hi'], body='whatever...')
+    '* :hi:\n whatever...'
+    >>> as_org_entry(heading=None, todo=None, tags=(), level=2)
+    '** '
+    >>> as_org_entry(heading='heading', body=None)
+    '* heading'
+    >>> as_org_entry(heading='heading', body='keep\n newlines\n')
+    '* heading\n keep\n  newlines\n'
+    >>> as_org_entry(heading='123', todo='TODO', level=0)
+    'TODO 123'
+    >>> as_org_entry(heading='*abacaba', body='***whoops', tags=('baa@d tag', 'goodtag'))
+    '* *abacaba :baa@d_tag:goodtag:\n ***whoops'
+    """
+    # TODO not great that we always pad body I guess. maybe needs some sort of raw_body argument?
+    # TODO FIXME escape everything properly!
     if heading is None:
-        if body is None:
-            raise RuntimeError('Both heading and body are empty!!')
-        heading = body.splitlines()[0] # TODO ??
+        heading = ''
+    heading = re.sub(r'\s', ' ', heading)
 
+    # TODO remove newlines from body?
     if body is not None:
         body = sanitize_org_body(body)
 
-    # TODO FIXME escape everything properly!
-    heading = re.sub(r'\s', ' ', heading)
-    # TODO remove newlines from body
 
     parts = []
 
@@ -96,20 +108,20 @@ def as_org_entry(
     if todo is not None:
         parts.append(todo)
 
-    # TODO hacky, not sure...
-    # sch = [f'  SCHEDULED: <{date2org(NOW)}>'] if todo == 'TODO' else []
+    if len(heading) > 0:
+        parts.append(heading)
 
-    props = {} if properties is None else properties
+    if len(tags) > 0:
+        # tags_s = ('' if heading.endswith(' ') else ' ') +
+        tags_s = ':' + ':'.join(map(sanitize_tag, tags)) + ':'
+        parts.append(tags_s)
 
-    parts.append(heading)
-
-    tags_s = ':'.join(map(sanitize_tag, tags))
-    if len(tags_s) > 0:
-        parts.append(f':{tags_s}:')
-
-    sch_lines = [] if scheduled is None else ['SCHEDULED: ' + org_dt(scheduled, active=True)]
+    sch_lines = [] if scheduled is None else [
+        'SCHEDULED: ' + org_dt(scheduled, active=True)
+    ]
 
     props_lines: List[str] = []
+    props = {} if properties is None else properties
     if len(props) > 0:
         props_lines.append(':PROPERTIES:')
         props_lines.extend(f':{prop}: {value}' for prop, value in props.items())
@@ -117,7 +129,9 @@ def as_org_entry(
 
     body_lines = [] if body is None else [body]
 
-    # TODO not sure... not super consistent
+    if len(parts) == 1:
+        # means it's only got level stars, so we need to make sure space is present (otherwise it's not an outline)
+        parts.append('')
     lines = [
         ' '.join(parts), # TODO just in case check that parts doesn't have newlines?
         *sch_lines,
@@ -127,69 +141,10 @@ def as_org_entry(
     # TODO FIXME careful here, I guess actually need some tests for endlines
     return '\n'.join(lines)
 
-
-def test_as_org_entry():
-    # shouldn't crash at least
-    as_org_entry(heading=None, tags=['hi'], body='whatever...', created=None, todo=None)
-
-
-def test_as_org_entry_0():
-    eee = as_org_entry(heading='123', created=None, todo='TODO', level=0)
-    assert eee.startswith('TODO 123')
-
-
-def test_santize():
-    ee = as_org_entry(
-        heading='aaaaa',
-        body='**** what about that?',
-        tags=['ba@@d tag', 'goodtag'],
-        force_no_created=True,
-        todo=None,
-    )
-    assert ee == """
-* aaaaa :ba@@d_tag:goodtag:
- **** what about that?
-""".strip()
-
-
-def test_body():
-    b = as_org_entry(
-        heading='heading',
-        body='please\nkeep newlines\n',
-        force_no_created=True,
-        todo=None,
-    )
-    assert b == """* heading
- please
- keep newlines
-"""
-
-
-def test_no_body():
-    b = as_org_entry(
-        heading='heading',
-        body=None,
-        force_no_created=True,
-        todo=None,
-    )
-    assert b == """* heading"""
-
-
-def test_todo():
-    b = as_org_entry(
-        heading='hi',
-        todo='TODO',
-        force_no_created=True,
-        body='fewfwef\nfewfwf'
-    )
-    assert b.startswith("""* TODO hi""")
-
-
 # TODO get rid of this
-def as_org(todo=None, inline_created=True, **kwargs):
+def as_org(todo=None, **kwargs):
     res = as_org_entry(
         todo=todo,
-        inline_created=inline_created,
         level=0,
         **kwargs,
     )
@@ -227,8 +182,6 @@ class OrgNode(NamedTuple):
             properties=self.properties,
             scheduled=self.scheduled,
             body=self.body,
-            force_no_created=True,
-            inline_created=True,
             level=0,
         )
 
